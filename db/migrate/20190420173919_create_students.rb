198582fb9797c42bf8e313202c29a846368dda80
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-04-20 17:40:05 +0000

    Automatically backed up by Learn

--- db/migrate/20190420173919_create_students.rb
+++ db/migrate/20190420173919_create_students.rb
@@ -2,10 +2,7 @@ class CreateStudents < ActiveRecord::Migration[5.0]
   def change
     create_table :students do |t|
       t.string :first_name
-      t.string :string
       t.string :last_name
-      t.string :string
-
       t.timestamps
     end
   end
